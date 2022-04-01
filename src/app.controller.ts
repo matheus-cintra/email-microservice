331eb3411b0844e5cd060f1eeebffa92b6d6a965
Matheus Cintra <matheus.cintra@synvia.com>
2022-04-01 15:02:00 -0300

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @EventPattern('email')
+  async notificate(@Payload() data: any): Promise<any> {
+    console.warn('send email', data);
+
+    return data;
+  }
+}
